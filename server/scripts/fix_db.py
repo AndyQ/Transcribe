ba664b9eedf5faa4daec9981d76a8fd6d3147516
Andy Qua <andy.qua@gmail.com>
2023-05-24 08:38:00 +0100

    fixdb script now creates table

--- server/scripts/fix_db.py
+++ server/scripts/fix_db.py
@@ -5,6 +5,22 @@ conn.row_factory = sqlite3.Row
 
 
 cur = conn.cursor()
+cur.execute('alter table item add column source_url text')
+cur.execute('alter table item rename to temp_item' )
+cur.execute("""CREATE TABLE item (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    type TEXT NOT NULL,
+    file_name TEXT NOT NULL,
+    source_url TEXT,
+    transcription_file TEXT,
+    status TEXT NOT NULL,
+    status_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+""")
+cur.execute('insert into item(id, created, title, type, file_name, source_url, transcription_file, status, status_updated) select id, created, title, type, file_name, source_url, transcription_file, status, status_updated from temp_item')
+cur.execute('drop table temp_item')
 
 cur.execute('SELECT * FROM item where type = "youtube"')
 
