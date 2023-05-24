158d7816115f1a2bd036409aaadea1eab8fe9e89
Andy Qua <andy.qua@gmail.com>
2023-05-24 08:40:29 +0100

    Fixed filename

--- server/scripts/fix_db.py
+++ server/scripts/fix_db.py
@@ -26,7 +26,7 @@ cur.execute('SELECT * FROM item where type = "youtube"')
 
 rows = [dict(row) for row in cur.fetchall()]
 for row in rows:
-    source_url = f"https://www.youtube.com/watch?v={row['source_url']}"
+    source_url = f"https://www.youtube.com/watch?v={row['file_name']}"
     cur.execute('UPDATE item SET source_url = ? WHERE id = ?', (source_url, row['id'],))
 
 conn.commit()
